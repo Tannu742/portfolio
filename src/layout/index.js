f7293b19e32bfbf35940973b8238de5509564bce
tannu742 <tanisharajput9575@gmail.com>
2024-09-10 23:10:36 +0530

    change

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/header";
+import Footer from "../components/footer";
 
 const Root = ()=>{
     return(
         <>
         <Header/>
         <Outlet/>
+        <Footer/>
         </>
     )
 }
