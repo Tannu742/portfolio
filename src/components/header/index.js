85f18ed8339e0324f1d952db1246e04faab562dd
tannu742 <tanisharajput9575@gmail.com>
2024-09-13 10:21:38 +0530

    responsive

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import { Col, Container, Row } from 'react-bootstrap';
 import { Links } from '../../constants/data';
 import { handleScroll } from '../../utils';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
     const links = useRef([]);
@@ -10,26 +11,45 @@ const Header = () => {
     useEffect(() => {
         handleScroll(links.current);
     }, []);
+
     return (
-        <header className="header banner">
+        <Navbar collapseOnSelect expand="lg" className="header">
             <Container>
-                <Row className='p-4'>
-                    <Col className='logo col-3'>
-                        <h5 className='fw-bolder'>TANISHA</h5>
-                    </Col>
-                    <Col className='col-9 navLinks align-content-center text-align-end'>
+                <Navbar.Brand href="" className='fw-bolder logo'>TANISHA</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
                         {
                             Links.map((item, i) => {
                                 return (
-                                    <Link ref={el => links.current[i] = el} to={item.path} key={item.id} className='head_links'>{item.link}</Link>
+                                    <Nav.Link ref={el => links.current[i] = el} href={item.path} key={item.id} className='head_links navLinks'>{item.link}</Nav.Link>
                                 )
                             })
                         }
-                    </Col>
-                </Row>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
-        </header>
+        </Navbar>
     );
 }
+
+// <header className="header banner">
+//     <Container>
+//         <Row className='p-4'>
+//             <Col className='logo col-3'>
+//                 <h5 className='fw-bolder'>TANISHA</h5>
+//             </Col>
+//             <Col className='col-9 navLinks align-content-center text-align-end'>
+//                 {
+//                     Links.map((item, i) => {
+//                         return (
+//                             <Link ref={el => links.current[i] = el} to={item.path} key={item.id} className='head_links'>{item.link}</Link>
+//                         )
+//                     })
+//                 }
+//             </Col>
+//         </Row>
+//     </Container>
+// </header>
 
 export default Header;
